6421e46684e2722d26f037568096b2a7dd4fb79e
Sam <samuel.fitz@btinternet.com>
2022-01-30 15:10:07 +0000

    Login and Homepage Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import {Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './containers/Home';
 
 const App = () => {
 
   return (
-<Routes>
-  <Route path="login" element={<Login />} />
-  <Route path="/*" element={<Home />} />
-</Routes>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
 };
 
