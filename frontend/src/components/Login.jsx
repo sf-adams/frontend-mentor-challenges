5e68212d31a2d18eb9de1b866701fca69faa4378
Sam <samuel.fitz@btinternet.com>
2022-02-04 16:13:20 +0000

    Login Background Added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import shareVide from "../assets/share.mp4";
+import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          Loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   )
 }
