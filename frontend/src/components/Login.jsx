5d0c1504b5bdb53b18d87bb2c5dbc184d11182ca
Sam <samuel.fitz@btinternet.com>
2022-02-05 22:56:11 +0000

    Logo Added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -19,7 +19,15 @@ const Login = () => {
           className="w-full h-full object-cover"
         />
 
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay"></div>
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+
+
+        </div>
+
+
       </div>
     </div>
   )
